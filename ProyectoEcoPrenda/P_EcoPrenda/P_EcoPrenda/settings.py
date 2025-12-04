fd4ce3fe630f1392eeb6de2dc609a67edbb4cada
KrisSs-ConShiLoHe <solwayfirth56@gmail.com>
2025-12-04 10:13:52 -0300

    cambios

--- ProyectoEcoPrenda/P_EcoPrenda/P_EcoPrenda/settings.py
+++ ProyectoEcoPrenda/P_EcoPrenda/P_EcoPrenda/settings.py
@@ -21,11 +21,19 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+import secrets
+print(secrets.token_urlsafe(50))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Carga las variables del archivo .env
 load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+# SECRET_KEY
+SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY no está definida en .env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
